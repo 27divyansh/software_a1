19cc656f31cdeb2de74abed6c309560a85a57933
27divyansh <divyanshkankariya@gmail.com>
2021-07-28 00:42:37 +0530

    code added

---
+++ auv_ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/divyansh/auv_ws/src
+# Build directory: /home/divyansh/auv_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("demo_bot_description")
+subdirs("demo_bot_gazebo")
